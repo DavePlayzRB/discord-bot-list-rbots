4e91096e367a9dd3129a09b0540e5813be2c71ff
Glitch (understood-incandescent-wanderer) <none>
2021-01-26 15:01:28 +0000

    🐬🍖 Checkpoint ./src/bot/commands/Bots/remove.js:24646111/796 ./src/dynamic/index.pug:24646111/36 ./src/public/assets/js/cards.js:24646111/9 ./src/dynamic/search.pug:24646111/100

--- src/bot/commands/Bots/remove.js
+++ src/bot/commands/Bots/remove.js
@@ -8,7 +8,7 @@ const reasons = {
     "1": `Your bot was offline when we tried to verify it.`,
     "2": `Your bot is a clone of another bot`,
     "3": `Your bot responds to other bots`,
-    "4": `Your bot doesn't have any/enough working commands. (Minimum: 7)`,
+    "4": `Your bot doesn't have any enough working commands. (Minimum: 7)`,
     "5": `Your bot has NSFW commands that work in non-NSFW marked channels`,
     "6": `Your bot doesn't have a working help command or commands list`
 }

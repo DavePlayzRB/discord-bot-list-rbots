1fcdf336c2d1269b021c1d51184782232ecbe3f2
Glitch (understood-incandescent-wanderer) <none>
2021-01-27 04:24:02 +0000

    🎩✋ Checkpoint ./src/bot/commands/Bots/queue.js:24646111/36

--- src/bot/commands/Bots/queue.js
+++ src/bot/commands/Bots/queue.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
         let cont = "";
         let bots = await Bots.find({ state: "unverified" }, { _id: false })
 
-        bots.forEach(bot => { cont += `<@${bot.botid}> : [Invite](https://discord.com/oauth2/authorize?client_id=${bot.botid}&scope=bot&guild_id=${id}&permissions=0)\n` })
+        bots.forEach(bot => { cont += `<@${bot.botid}> : [Invite Bot](https://discord.com/oauth2/authorize?client_id=${bot.botid}&scope=bot&guild_id=${id}&permissions=0)\n` })
         if (bots.length === 0) cont = " No Bots Here!";
 
         let embed = new MessageEmbed()

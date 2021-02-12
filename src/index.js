294e4fb8dbcc62adbb2b1c5af687e301711a1754
Glitch (rbots) <none>
2021-02-12 00:57:09 +0000

    💨🐓 Checkpoint ./src/index.js:24646111/851

--- src/index.js
+++ src/index.js
@@ -19,3 +19,23 @@ const { web: {port}, discord_client: {token}, mongo_url } = require("@root/confi
     await new App(client).listen(port || 8080);
     console.log(`Running on port ` + `\x1b[34m\x1b[4m${port || 80}\x1b[0m`);
 })()
+
+
+var express = require('express')
+var app = express()
+
+function checkHttps(req, res, next){
+  // protocol check, if http, redirect to https
+  
+  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+    console.log("https, yo")
+    return next()
+  } else {
+    console.log("just http")
+    res.redirect('https://' + req.hostname + req.url);
+  }
+}
+
+app.all('*', checkHttps)
+app.use(express.static('public'))
+app.listen(8000)

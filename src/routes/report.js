571b7f132699759a51934b9221b977747c78a7c9
Glitch (rbots) <none>
2021-02-14 08:14:31 +0000

    🎭🌇 Checkpoint ./src/dynamic/bots.pug:24646111/47 ./src/routes/report.js:24646111/582

--- src/routes/report.js
+++ src/routes/report.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
+const { auth } = require('@utils/discordApi');
+
+const { web: {recaptcha_v2: {site_key}}, bot_options: {bot_tags, max_summary_length} } = require("@root/config.json");
 
 const route = Router();
 
-route.get("/", async (req, res) => {
-    res.render("report", {req})
+route.get("/", auth, async (req, res) => {
+    res.render("report", {
+        bot_tags,
+        max_summary_length,
+        site_key,
+        req
+    })
 });
 
 module.exports = route;
